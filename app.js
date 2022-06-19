582c66680a9a8ecb7fc39a1929c7816fdb71508c
Tran De <dedi20v7x197@dttx.ctu.edu.vn>
2022-06-19 23:43:04 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-
 const contactsRouter = require("./routes/contact.route");
+const ApiError = require("./api-error");
+
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
@@ -11,5 +13,14 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+    });
+    app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    });
+});
+
 
 module.exports = app;
