e388c02e158af526f70eeff879e335b94e8d938d
Tran De <dedi20v7x197@dttx.ctu.edu.vn>
2022-06-19 22:58:07 +0700

    Cai dat cac route cho contect

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+
+const contactsRouter = require("./routes/contact.route");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Chao mung den ung dung" });
 });
+
+app.use("/api/contacts", contactsRouter);
+
 module.exports = app;
